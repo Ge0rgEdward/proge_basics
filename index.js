224618bdd9412d316c59ab1763f49f10571279c9
Georg Edward <georgedawrd.piil@gmail.com>
2024-09-27 07:40:43 -0500

    JavaScript basics

---
+++ index.js
@@ -0,0 +1,49 @@
+// See on mu esimene JavaScript kood!
+console.log('Hello World');
+let name = 'Piil'; //string literal
+console.log(name);
+let interestRate=0.3;
+interestRate=1;
+console.log('interestRate')
+let age=30; //number literal 
+let isApproved=true; // Boolean literal
+let firstName= undefined;
+let selectedColor = null;
+//let muutujat saab muuta
+//const muutujat ei saa muuta
+
+let person = {
+    age: 30,
+    name: 'Piil',
+}
+
+
+person.name = 'John';
+person[name] ='Mary'
+
+
+console.log(person.name);
+
+let valitudVärvid = ['red','blue']
+selectedColors[2]='green';
+
+
+console.log(selectedColors.length);
+
+
+//Codeguage ylesanne 
+
+let a = prompt('a=...');
+let b = prompt('b=...');
+
+a= Number(a);
+b= Number(b);
+
+let sum= a+b;
+
+document.write('Summa on:'+ sum);
+
+
+
+
+
---
+++ näidiskalkulaator.js
@@ -0,0 +1,9 @@
+let a = prompt('a=...');
+let b = prompt('b=...');
+
+a= Number(a);
+b= Number(b);
+
+let sum= a+b;
+
+document.write('Summa on:'+ sum);
